7242692b8ce84fa3d1da4abc7025957ee4ff48e9
刘凤玉 <liufengyu@liufengyudeMacBook-Pro.local>
2017-03-16 11:22:38 +0800

    初始化

---
+++ src/main/java/liufybj/App.java
@@ -0,0 +1,13 @@
+package liufybj;
+
+import liufybj.scala.HelloWorld;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        HelloWorld.print();
+    }
+
+}
---
+++ src/main/scala/liufybj/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+package liufybj.scala
+
+/**
+  * Created by liufengyu on 2017/3/16.
+  */
+object HelloWorld {
+  def print() = {
+    println("I love scala");
+  }
+}
